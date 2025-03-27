0407ef3fbf446aac52c6f84b7b06f57ee9dc2b37
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-03-26 22:13:48 -0400

    Add error handling

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+/* Environment */
+const host = '127.0.0.1';
+const PORT = 8080;
 
 // app.use()
 app.use(express.static('public'));
@@ -48,6 +54,30 @@ app.get('/movies', (req, res) => {
   res.json(bestMovies);
 });
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// app.get('/test-error', (req, res, next) => {
+//   try {
+//     throw new Error('Error handling works');
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send('This should not be happening!');
+// });
+
+app.get('*', (req, res) => {
+  res.status(404).send('Error 404: Page not found');
+});
+
 app.listen(8080, () => {
-  console.log('Listening on port 8080.')
+  console.log(`Server running at http://${host}:${PORT}`);
 });
