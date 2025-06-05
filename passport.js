611ece67f677653b4a2abeb44707d6bb5e868987
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-06-05 12:15:03 -0400

    Update passport.js to prevent NoSQL injections

--- passport.js
+++ passport.js
@@ -1,11 +1,13 @@
+const { default: mongoose } = require('mongoose');
+
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models'),
   passportJWT = require('passport-jwt');
 
-let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJwt;
+const Users = Models.User;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(
   new LocalStrategy(
@@ -14,8 +16,14 @@ passport.use(
       passwordField: 'password'
     },
     async (username, password, callback) => {
-      await Users.findOne({ username: username})
-      .then((user) => {
+      try {
+        // Validate input format to prevent NoSQL injections
+        if (typeof username !== 'string' || !/^[a-zA-Z0-9]+$/.test(username)) {
+          return callback(null, false, { message: 'Invalid username format' });
+        }
+
+        const user = await Users.findOne({ username: username.trim() });
+
         if (!user) {
           console.log('Incorrect username');
           return callback(null, false, { message: 'Incorrect username' });
@@ -26,33 +34,40 @@ passport.use(
           return callback(null, false, { message: 'Incorrect password' });
         }
 
-        console.log('Finished');
+        console.log('Login successful');
         return callback(null, user);
-      })
-      .catch((error) => {
-        if (error) {
-          console.log(error);
-          return callback(error);
-        }
-      });
+      }
+      catch {
+        console.log(error);
+        return callback(error);
+      }
     }
   )
 );
 
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
-}, async (jwtPayload, callback) => {
-  try {
-    const user = await Users.findById(jwtPayload._id);
-    if (user) {
-      return callback(null, user);
-    } 
-    else {
-      return callback(null, false);
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'your_jwt_secret'
+    }, 
+    async (jwtPayload, callback) => {
+      try {
+        if (!mongoose.Types.ObjectId.isValid(jwtPayload._id)) {
+          return callback(null, false);
+        }
+
+        const user = await Users.findById(jwtPayload._id);
+        if (user) {
+          return callback(null, user);
+        } 
+        else {
+          return callback(null, false);
+        }
+      } 
+      catch (error) {
+        return callback(error);
+      }
     }
-  } 
-  catch (error) {
-    return callback(error);
-  }
-}));
+  )
+);
